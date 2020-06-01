9bb68bec5bfb071d407b07f827ff8a5f9934eb27
Runebjepsen <34272890+Runebjepsen@users.noreply.github.com>
2020-06-01 17:34:32 +0200

    Basic UI

---
+++ Assets/Scenes/GameNavigation.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameNavigation : MonoBehaviour
+{
+    public GameObject Menu;
+    public GameObject Inventory;
+    public GameObject Settings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //FixedUpdate()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Menu.activeSelf)
+            {
+                Menu.SetActive(false);
+            }
+            else
+                Menu.SetActive(true);
+                Inventory.SetActive(false);
+        }
+        if (Input.GetKeyDown("i"))
+        {
+            if (!Menu.activeSelf)
+            {
+                if (Inventory.activeSelf)
+                {
+                    Inventory.SetActive(false);
+                }
+                else
+                    Inventory.SetActive(true);
+            }
+
+        }
+    }
+
+    public void OpenSettings()
+    {
+        Settings.SetActive(true);
+        Menu.SetActive(false);
+    }
+    public void OpenMenu()
+    {
+        Menu.SetActive(true);
+        Settings.SetActive(false);
+    }
+    public void CloseMenu()
+    {
+        Menu.SetActive(false);
+    }
+    public void CloseInventory()
+    {
+        Inventory.SetActive(false);
+    }
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scenes/MenuNavigation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject MainSettings;
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void OpenSettings()
+    {
+        MainMenu.SetActive(false);
+        MainSettings.SetActive(true);
+    }
+    public void OpenMainMenu()
+    {
+        MainMenu.SetActive(true);
+        MainSettings.SetActive(false);
+    }
+}
